a6d79195e1922320d669738b855a93c6b2656211
karthik8402 <karthikumar8402@gmail.com>
2026-02-23 13:53:59 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./index.html", // Adjust paths according to your HTML location
+    "./**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
